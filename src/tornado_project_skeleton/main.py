e6abc3ecdae03670a047cf10945b938fa93220a4
Paweł Suder <pawel@suder.info>
2015-03-23 22:04:38 +0100

    Add simple template

--- src/tornado_project_skeleton/main.py
+++ src/tornado_project_skeleton/main.py
@@ -1,4 +1,6 @@
 import os
+import tornado.ioloop
+import tornado.web
 
 from tornado_project_skeleton.tools import config
 
@@ -6,5 +8,24 @@ from tornado_project_skeleton.tools import config
 pwd = os.path.dirname(os.path.abspath(__file__))
 config.add_config_ini('%s/main.ini' % pwd)
 
-if __name__ == '__main__':
-    print 'Hello World!\nValue for NAME is %s.' % config.NAME
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        name = config.NAME
+        items = ['smart', 'clever', 'intuitive']
+        kwargs = {'title': 'My title',
+                  'name': name,
+                  'items': items}
+        self.render('template.html', **kwargs)
+
+    def data_received(self, chunk):
+        pass
+
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
