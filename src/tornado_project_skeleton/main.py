720b31ce0b344524c81cbfb23c073680f770fe9f
Paweł Suder <pawel@suder.info>
2015-03-23 22:28:52 +0100

    Basic stuff with Tornado and Bootstrap

--- src/tornado_project_skeleton/main.py
+++ src/tornado_project_skeleton/main.py
@@ -1,4 +1,5 @@
 import os
+import tornado.autoreload
 import tornado.ioloop
 import tornado.web
 
@@ -22,10 +23,15 @@ class MainHandler(tornado.web.RequestHandler):
         pass
 
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+application = tornado.web.Application(
+    handlers=[
+        (r'/', MainHandler),
+        (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': '%s/static/' % pwd}),
+    ])
 
 if __name__ == "__main__":
     application.listen(8888)
+    tornado.autoreload.start()
+    tornado.autoreload.watch('%s/template.html' % pwd)
+    tornado.autoreload.watch('%s/static/style.css' % pwd)
     tornado.ioloop.IOLoop.instance().start()
